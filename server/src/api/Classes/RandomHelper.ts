0b21142bf6ab70e080db03cef686e094d51f8afb
Anirudh Mungre <anirudh.mungre16@gmail.com>
2020-03-19 23:48:53 -0400

    ignore and remove

--- server/src/api/Classes/RandomHelper.ts
+++
@@ -1,5 +0,0 @@
-export class RandomHelper {
-    public static randomFloat(minimum: number = 0, maximum: number = 1): number {
-        return Math.random() * (maximum - minimum) + minimum;
-    }
-}
